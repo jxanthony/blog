ffd5dfde80e66e81484d14e9796a88caaebea3b8
jxanthony <jxanthony@gmail.com>
2015-07-30 07:03:44 -0700

    last commit, blog done. added (plaintext) auth. should look into something like Devise or Warden for auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "secret", password: "secret", except: [:index, :show, :new, :create]
+
   def index
     @articles = Article.all
   end
